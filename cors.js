77fc99c2bee5c2325cbdf1897abfa46bffe8858c
selenasmall <selenawiththetattoo@gmail.com>
2019-01-07 17:47:43 +1100

    CORS config to allow post requests to the server

--- cors.js
+++ cors.js
@@ -1,7 +1,12 @@
 module.exports = function() {
   return function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
   };
 }
