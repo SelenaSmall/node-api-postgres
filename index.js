a785c168dbf3fa91db8f7ef903f28e8a5d851220
selenasmall <selenawiththetattoo@gmail.com>
2019-01-07 14:45:50 +1100

    Use express to serve static files from React app - stored in 'client/build' on production

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 5000
@@ -6,6 +7,9 @@ const db = require('./queries');
 const cors = require('./cors');
 
 app.use(cors());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.json())
 app.use(
@@ -23,6 +27,13 @@ app.get('/comments/:id', db.getCommentById)
 app.post('/comments', db.createComment)
 app.put('/comments/:id', db.updateComment)
 app.delete('/comments/:id', db.deleteComment)
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
