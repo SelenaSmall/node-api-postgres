209f3f2d16469145751ee5cb42d963fafe39fc0e
selenasmall <selenawiththetattoo@gmail.com>
2019-01-09 15:23:18 +1100

    Add columns to comments tables and make database.js mildly more usable
    # TODO many improvements to be made here

--- models/database.js
+++ models/database.js
@@ -1,10 +1,49 @@
+// Run this file to create and update tables in your database
+// $ node models/database.js buildTables
+
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/node-api-postgres';
-
 const client = new pg.Client(connectionString);
-client.connect();
 
-// Create table in your database
-const query = client.query(
-  'CREATE TABLE comments(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, author VARCHAR(40))');
-query.on('end', () => { client.end(); });
+const buildTables = () => {
+  client.connect();
+  createCommentsTable();
+  addCreatedAtAndUpdatedAtToCommentsTable();
+};
+
+const clientQuery = (queryText) => {
+  client.query(queryText)
+    .then((res) => {
+      console.log(res);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+};
+
+const createCommentsTable = () => {
+  clientQuery(
+    `CREATE TABLE IF NOT EXISTS
+      comments(
+        id SERIAL PRIMARY KEY, 
+        text VARCHAR(40) not null, 
+        author VARCHAR(40)
+      )`
+  );
+};
+
+const addCreatedAtAndUpdatedAtToCommentsTable = () => {
+  clientQuery(
+    `ALTER TABLE IF EXISTS comments 
+      ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT NOW(),
+      ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP DEFAULT NOW();`
+  );
+};
+
+module.exports = {
+  buildTables,
+};
+
+require('make-runnable');
